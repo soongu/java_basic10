46c6d040985860aa53ac4d4955edf4b493b487b3
soongu <naong526@naver.com>
2022-10-09 15:18:07 +0900

    Initial commit

---
+++ src/main/java/com/spring/oop/inherit/Warrior.java
@@ -0,0 +1,26 @@
+package com.spring.oop.inherit;
+
+public class Warrior extends Player {
+
+    private int rage;
+
+
+    public Warrior() {
+        super();
+        System.out.println("Warrior 기본 생성자 호출!");
+    }
+
+    public Warrior(String nickName) {
+        super(nickName);
+        System.out.println("Warrior 생성자 호출!");
+        this.rage = 100;
+    }
+
+    @Override
+    public void info() {
+        super.info();
+        System.out.println("# 분노: " + rage);
+
+    }
+
+}
---
+++ src/test/java/com/spring/oop/encap/CarTest.java
@@ -0,0 +1,22 @@
+package com.spring.oop.encap;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @Test
+    void carTest() {
+
+        Car car = new Car("포르쉐911", 320);
+
+        car.on();
+
+        for (int i = 0; i < 10; i++) {
+            car.accelerator();
+        }
+
+    }
+
+}
